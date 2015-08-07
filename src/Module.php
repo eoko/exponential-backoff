94b5b4af52263125ce10eef289be67bbf8290f12
merlin <romain.dary@eoko.fr>
2015-08-07 13:40:05 +0200

    Add module for zf2 compat

--- src/Module.php
+++ src/Module.php
@@ -6,7 +6,7 @@ class Module
     public function getConfig()
     {
         return [
-            'controllers' => [
+            'service_manager' => [
                 'invokables' => [
                     'Eoko\ExponentialBackoff' => 'Eoko\ExponentialBackoff\Utils\ExponentialBackoff',
                 ],
@@ -19,7 +19,7 @@ class Module
         return [
             'Zend\Loader\StandardAutoloader' => [
                 'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src',
+                    __NAMESPACE__ => __DIR__,
                 ],
             ],
         ];
